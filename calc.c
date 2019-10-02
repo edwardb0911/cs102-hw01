7caae52a3ac4a48940ecb6da6f7e7035cb6a1df7
root <root@LAPTOP-REOMIDGU.localdomain>
2019-10-02 19:57:31 -0400

    adding works

--- calc.c
+++ calc.c
@@ -5,8 +5,15 @@
 int main(int argc, char *argv[]){
 	if (argc != 4) {
     printf("Wrong number of arguments!\n");
+	printf("%s\n", argv[1]);
     return(-1); // nonzero exit code indicates problem
    	}
+	else{
+		if (argv[1][0] == '+'){
+			float x = (atof(argv[2]) + atof(argv[3]));
+			printf("%f\n",  x);
+		}
+	}
 
 
 
