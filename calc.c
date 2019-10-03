c2e8997b67c844ead72583fbf528e13b14efe772
root <root@LAPTOP-REOMIDGU.localdomain>
2019-10-03 19:37:59 -0400

    works but is a little too precise

--- calc.c
+++ calc.c
@@ -5,36 +5,32 @@
 int main(int argc, char *argv[]){
 	if (argc != 4) {
     printf("Wrong number of arguments!\n");
-	printf("%s\n", argv[1]);
     return(-1); // nonzero exit code indicates problem
    	}
 	else{
 		if (argv[1][0] == '+'){
 			float x = (atof(argv[2]) + atof(argv[3]));
 			printf("%f\n",  x);
+			return 0;
 		}
 		if (argv[1][0] == '-'){
 			float x = (atof(argv[2]) - atof(argv[3]));
 			printf("%f\n",  x);
+			return 0;
 		}
 		if (argv[1][0] == 'x'){
 			float x = (atof(argv[2]) * atof(argv[3]));
 			printf("%f\n",  x);
+			return 0;
 		}
 		if (argv[1][0] == '/'){
 			float x = (atof(argv[2]) / atof(argv[3]));
 			printf("%f\n",  x);
+			return 0;
 		}
 	}
-
-
-
-
-
-
-
-
-return 0;
+	printf("Invalid Operator \n");
+	return -1;
 }
 
 
